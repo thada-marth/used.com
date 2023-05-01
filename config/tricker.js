6ea1ba17539ed4e47a7e92ded4500ba96dd3775b
Mx-Pc <marthxer@gmail.com>
2023-05-01 19:25:10 +0700

    edit prod url env

--- config/tricker.js
+++ config/tricker.js
@@ -1,2 +1,2 @@
 const dev = process.env.NODE_ENV !== 'production';
-export const server = dev ? 'http://localhost:3000' : 'https://used.marth.me';
+export const server = dev ? 'http://localhost:3000' : `https://${process.env.VERCEL_URL}`;
